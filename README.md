89febfdbf2f321e40097d7a7745d46871662b9aa
Monsoon Co <monsoonco@Monsoons-MacBook-Pro.local>
2018-06-03 21:31:54 -0700

    Added all js html npm package files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # FriendFinder
+Overview
+In this activity, you'll build a compatibility-based "FriendFinder" application -- basically an app that finds you compaitable with a famous basketball player. This full-stack site will take in results your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+#Technologies Used
+HTML
+CSS
+Javascript
+Express
+Node
+JSON
---
+++ app/data/friends.js
@@ -0,0 +1,43 @@
+// basketball players data
+var ballersData = [
+    {
+      name: "Kobe Bryant",
+      photo: "https://b.fssta.com/uploads/application/nba/players/3100.vresize.350.425.medium.46.png",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Lebron James",
+      photo:"https://i1.wp.com/www.nationalreview.com/wp-content/uploads/2018/05/RTX66A5E.jpg?fit=789%2C460&ssl=1",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Magic Johnson",
+      photo:"http://www.latimes.com/resizer/5RoTzCXTddBP0FC6SG_7HAp-oMU=/1400x0/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/UZ24XKTDERAKFHI5KIUF2B77ME.jpg",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Steph Curry",
+      photo:"https://images.tmz.com/2017/06/14/0614-stephen-curry-profile-200x250.jpg",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Kevin Durant",
+      photo:"http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3202.png&w=350&h=254",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Shaquille O'Neal",
+      photo:"http://www.6am-group.com/wp-content/uploads/2015/07/shaq-feat.jpg",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "Michael Jordan",
+      photo:"https://naqyr37xcg93tizq734pqsx1-wpengine.netdna-ssl.com/wp-content/uploads/2016/01/75-Motivational-Michael-Jordan-Quotes.jpg",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+
+  module.exports = ballersData;
+  
+  
+  
